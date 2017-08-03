c5a9df2e975f609cd4e60015352cf9defc0e9727
Chris <38916720+xscore@users.noreply.github.com>
2017-08-03 16:20:14 +0200

    Fix installer and client being started multiple times
    Now starts the client binary directly instead of the startscript, to avoid creating a new client instance every time a ts3server link is clicked
    Due to the client binary still having the platform suffix, this required duplicating the desktop file
    
    Fixed Installer to detect architecture and use the proper .desktop file

--- installAssoc.sh
+++ installAssoc.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+baseDir=$(dirname "${BASH_SOURCE[0]}")
+arch=$(uname -m | grep '64')
+if [ $arch != '' ]; then
+	arch=amd64
+else
+	arch=x86
+fi
 
-xdg-icon-resource install --size 128 teamspeak-appIcon_128.png teamspeak-client
-xdg-icon-resource install --size 128 --context mimetypes teamspeak-appIcon_128.png application-x-ts3-addon
-xdg-icon-resource install --size 48 teamspeak-appIcon_48.png teamspeak-client
-xdg-icon-resource install --size 48 --context mimetypes teamspeak-appIcon_48.png application-x-ts3-addon
-xdg-mime install teamspeak-ts3AddonMime.xml
-xdg-desktop-menu install teamspeak-ts3Addon.desktop
-xdg-desktop-menu install teamspeak-ts3Client.desktop
+xdg-icon-resource install --size 128 ${baseDir}/teamspeak-appIcon_128.png teamspeak-client
+xdg-icon-resource install --size 128 --context mimetypes ${baseDir}/teamspeak-appIcon_128.png application-x-ts3-addon
+xdg-icon-resource install --size 48 ${baseDir}/teamspeak-appIcon_48.png teamspeak-client
+xdg-icon-resource install --size 48 --context mimetypes ${baseDir}/teamspeak-appIcon_48.png application-x-ts3-addon
+xdg-mime install ${baseDir}/teamspeak-ts3AddonMime.xml
+xdg-desktop-menu install ${baseDir}/teamspeak-ts3Addon.desktop
+xdg-desktop-menu install ${baseDir}/teamspeak-ts3Client_${arch}.desktop
